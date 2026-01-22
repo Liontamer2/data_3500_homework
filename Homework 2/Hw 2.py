4d10e534423a6143d6fee65c6bb50065878758f6
Liontamer2 <93547791+Liontamer2@users.noreply.github.com>
2026-01-22 22:31:03 +0000

    Push 1 for Homework 2 - Incomplete

---
+++ Homework 2/Hw 2.py
@@ -0,0 +1,38 @@
+# 2.3 Fill missing code that prints message 
+grade = 100 #int(input("What is your grade in the class: ")) #asks for grade in put and makes it an int
+# if else statement to tell them if they got an A or just did well 
+if grade >= 90: 
+    print("Congratulations! Your grade of 91 earns you an A in this course") 
+else:
+    print("You still did well!")
+
+# 2.4 Artimetic 
+add = 27.5 + 2 # Addition Calculation
+print("27.5 + 2","=", add)
+subtract = 27.5 - 2 #Subtraction Calculation
+print("27.5 - 2", "=", subtract)
+multiply = 27.5 * 2 #Multiplication Calculation
+print("27.5 * 2","=", multiply)
+divide = 27.5 / 2 #Division Calculation
+print("27.5 / 2","=", divide)
+floor_divide = 27.5 // 2 #Floor Division Calculation
+print("27.5 // 2","=", floor_divide)
+exponent = 27.5 ** 2 #Exponent Calculation
+print("27.5 ** 2","=", exponent)
+
+# 2.5 Circle Area, Diameter, and Circumference
+diameter = 2 * 2 # Diameter Calculation
+print("The diameter of the circle is", diameter)
+circumference = 2 * 3.14159 * 2  # Circumference Calculation
+print('The circumference of the circle is', circumference)
+area = 3.14159 * (2 ** 2) # Area Calculation
+print("The area of the circle is", area)
+
+# 2.6 Odd or Even 
+odd_or_even = int(input("Please give me a number: " )) # Asking for a number 
+if odd_or_even % 2 == 0: # Calculating using remainder to find odd or even
+    print(odd_or_even, "is a even number.") # Telling them it is a even number
+else:
+    print(odd_or_even, "is and odd number") # Telling them it is a odd number
+
+# 2.7 Multiples 
